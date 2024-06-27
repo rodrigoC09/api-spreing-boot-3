8d9d2f21a7936d58da00f9a30177e1f06c770101
rodrigoC09 <rodrigo.antonio.caro@gmail.com>
2024-06-27 12:37:28 -0400

    primer commit

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,34 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.direccion.Direccion;
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+public class Paciente {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String email;
+    private String documentoIdentidad;
+    private String telefono;
+
+    @Embedded
+    private Direccion direccion;
+
+    public Paciente(DatosRegistroPaciente datos){
+        this.nombre = datos.nombre();
+        this.email = datos.email();
+        this.documentoIdentidad = datos.documentoIdentidad();
+        this.telefono = datos.telefono();
+        this.direccion = new Direccion(datos.direccion());
+    }
+}
