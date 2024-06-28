6397a3b6ade1779539d3fc4d1df9880a6f7af961
rodrigoC09 <rodrigo.antonio.caro@gmail.com>
2024-06-27 20:23:09 -0400

    actualizacion de la consulta con filtros

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findByActivoTrue(Pageable paginacion);
 
-
+    //actualizar query
     @Query("""
             select m from Medico m
             where m.activo= 1 
